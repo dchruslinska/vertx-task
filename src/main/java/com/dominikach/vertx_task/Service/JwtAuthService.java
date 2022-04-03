067dc974d63b0fa0c2f3b7a4c3484974707660dc
Dominika <dominikachruslinska98@gmail.com>
2022-04-03 16:24:48 +0200

    getToken response added

--- src/main/java/com/dominikach/vertx_task/Service/JwtAuthService.java
+++ src/main/java/com/dominikach/vertx_task/Service/JwtAuthService.java
@@ -2,10 +2,12 @@ package com.dominikach.vertx_task.Service;
 
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
+import lombok.extern.slf4j.Slf4j;
 
 import static com.dominikach.vertx_task.Verticle.MainVerticle.jwtAuthProvider;
 import static com.dominikach.vertx_task.Verticle.MainVerticle.mongoClient;
 
+@Slf4j
 public class JwtAuthService {
 
   public static void response(RoutingContext routingContext, int httpstatus) {
@@ -20,15 +22,18 @@ public class JwtAuthService {
     JsonObject loginMatch = new JsonObject().put("login", login);
     mongoClient.findOne("user", loginMatch, null, jsonObjectAsyncResult -> {
       if(jsonObjectAsyncResult.succeeded()){
-        if(jsonObjectAsyncResult == null){
+        if(jsonObjectAsyncResult.result() == null){
           response(routingContext, 400);
         } else {
           String userId = jsonObjectAsyncResult.result().getString("id");
           JsonObject token = new JsonObject().put("token", jwtAuthProvider.generateToken(new JsonObject().put("UUID", userId)));
-          String aaa = token.getString("token");
+          String tokenString = token.getString("token");
+          routingContext.response()
+            .putHeader("content-type", "application/json")
+            .setStatusCode(200)
+            .end(tokenString);
         }
       }
     });
   }
-
 }
