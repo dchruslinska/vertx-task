17524a866dd2d4093fb0a00a689b879b4168452f
Dominika <dominikachruslinska98@gmail.com>
2022-04-03 23:58:45 +0200

    ItemApi added

---
+++ src/main/java/com/dominikach/vertx_task/ItemApi.java
@@ -0,0 +1,11 @@
+package com.dominikach.vertx_task;
+
+import com.dominikach.vertx_task.Service.ItemService;
+import io.vertx.ext.web.RoutingContext;
+
+public class ItemApi {
+
+  public static void addItem(RoutingContext routingContext){ ItemService.addItem(routingContext); }
+  public static void showItems(RoutingContext routingContext) { ItemService.getItems(routingContext); }
+
+}
